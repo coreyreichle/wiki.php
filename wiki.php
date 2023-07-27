c34294db3d8beabdb83a619902110fed05aed6e4
coreyreichle <coreyreichle@users.noreply.github.com>
2023-07-27 13:56:48 -0400

    Added anti-WEI protection for users.

--- wiki.php
+++ wiki.php
@@ -95,6 +95,9 @@ echo $Parsedown->text($footer);
 print "	</div>
 <!-- End Footer -->
 
+<!-- Anti-WEI protection, to ensure user security is respected -->
+		<script>if(navigator.getEnvironmentIntegrity!==undefined)document.querySelector('body').innerHTML='<h1>Your browser contains Google DRM</h1>"Web Environment Integrity" is a Google euphemism for a DRM that is designed to prevent ad-blocking. In support of an open web, this website does not function with this DRM. Please install a browser such as <a href="https://www.mozilla.org/en-US/firefox/new/">Firefox</a> that respects your freedom and supports ad blockers.';</script>
+<!-- End Anti-WEI protection block -->
 	</body>
 </html>";
 ?>
